0850ad37773db3f8f270626e24e4000343a00d53
Siege lion <405843463@qq.com>
2023-03-22 18:20:17 +0800

    feat:图片二维码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,24 +1,55 @@
 <template>
   <view class="content">
-    <img src="../static/mp-weixin/login-bg.png" />
+    <img
+      class="bgimages"
+      src="../static/mp-weixin/login-bg.png" />
     <view class="text-area">
       <div class="text-name">高效机房智控平台</div>
       <div class="title-name-EN">Chiller Plant Intelligent Control System</div>
     </view>
     <div class="login-button">
-      <button class="wxlogin">微信用户一键登录</button>
+      <button
+        class="wxlogin"
+        open-type="getPhoneNumber"
+        @getphonenumber="getphonenumber">
+        微信用户一键登录
+      </button>
       <button
         class="phonelogin"
-        plain>
+        plain
+        @click="toLogin">
         切换手机号登陆
       </button>
+      <div
+        class="userargeement"
+        @click="handlegrement">
+        <img
+          v-if="argeement"
+          src="../static/mp-weixin/rij.png" />
+        <img
+          v-else
+          src="../static/mp-weixin/rig.png" />
+        <view class="user-text">已阅读并同意</view>
+        <a href="###">《》</a>
+      </div>
     </div>
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-const title = ref('Hello')
+const argeement = ref(false)
+const handlegrement = () => {
+  argeement.value = !argeement.value
+}
+const toLogin = () => {
+  uni.navigateTo({
+    url: '/pages/login/index',
+  })
+}
+const getphonenumber = (e: any) => {
+  console.log(e)
+}
 </script>
 
 <style lang="scss">
@@ -26,7 +57,7 @@ const title = ref('Hello')
   width: 100%;
   height: 100vh;
   overflow: hidden;
-  image {
+  .bgimages {
     z-index: -1;
     width: 100%;
     height: 100%;
@@ -67,6 +98,23 @@ const title = ref('Hello')
     opacity: 0.4;
     border: 2rpx solid #2681ff;
     color: #ffffff;
+    margin-bottom: 34rpx;
+  }
+}
+.user-text {
+  font-size: 24rpx;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #cccccc;
+  line-height: 34rpx;
+  margin-left: 16rpx;
+}
+.userargeement {
+  display: flex;
+  align-items: center;
+  image {
+    width: 40rpx;
+    height: 40rpx;
   }
 }
 </style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <div class="login-box">
+      <div class="login-text">登录/注册</div>
+      <div class="login-desc">欢迎使用海尔高效机房智控平台</div>
+    </div>
+    <div class="form-box">
+      <uni-easyinput
+        class="account"
+        prefixIcon="person"
+        v-model="account"
+        trim="all"
+        :inputBorder="false"
+        placeholder="请输入手机号码/账号"></uni-easyinput>
+      <uni-easyinput
+        class="password"
+        prefixIcon="locked"
+        v-model="password"
+        trim="all"
+        type="password"
+        :inputBorder="false"
+        placeholder="请输入登录密码">
+        <slot name="prefixIcon"></slot>
+      </uni-easyinput>
+      <button
+        class="login-button"
+        @click="login">
+        登 录
+      </button>
+      <div
+        class="miss-password"
+        @click="forgetpassword">
+        忘记密码
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const account = ref()
+const password = ref()
+const login = () => {
+  console.log(account, password)
+  uni.reLaunch({
+    url: '/pages/home-page/index',
+  })
+}
+const forgetpassword = () => {
+  uni.navigateTo({
+    url: '/pages/forget-password/index',
+  })
+}
+</script>
+<style lang="scss">
+.login-box {
+  margin-top: 80rpx;
+  margin-left: 48rpx;
+  .login-text {
+    font-size: 48rpx;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 500;
+    color: #333333;
+    line-height: 66rpx;
+    margin-bottom: 16rpx;
+  }
+  .login-desc {
+    font-size: 28rpx;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #666666;
+    line-height: 40rpx;
+  }
+}
+.form-box {
+  margin-top: 158rpx;
+  padding: 0 48rpx;
+  .uni-easyinput {
+    border-bottom: 2rpx solid #e5e5e5;
+    margin-bottom: 14rpx;
+  }
+  .uni-easyinput__content {
+    height: 96rpx;
+  }
+  .uni-easyinput__content-input {
+    font-size: 32rpx;
+  }
+  .login-button {
+    margin-top: 80rpx;
+    background-color: #2681ff;
+    color: #fff;
+  }
+  .miss-password {
+    margin-top: 32rpx;
+    font-size: 28rpx;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #666666;
+    line-height: 40rpx;
+    text-align: right;
+  }
+}
+</style>

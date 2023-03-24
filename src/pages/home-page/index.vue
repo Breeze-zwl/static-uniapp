6dd24f265dcc15143d9b2c0e78892e0f65fab91b
Siege lion <405843463@qq.com>
2023-03-24 16:53:33 +0800

    feat:首页静态页面完成

--- src/pages/home-page/index.vue
+++ src/pages/home-page/index.vue
@@ -9,7 +9,10 @@
           class="location"
           @click="handleChangeLocal">
           <img src="~@/static/mp-weixin/home-page/local.png" />
-          海尔空气楼c02
+          {{ chooselocal }}
+          <img
+            class="local-choose"
+            src="~@/static/mp-weixin/home-page/chooseselect.png" />
         </div>
         <div class="real-location">{{ addressNameCity }} {{ addressNameDistrict }}</div>
       </div>
@@ -24,30 +27,115 @@
           <img src="@/static/mp-weixin/home-page/warn.png" />
           <div>上次维保时间：</div>
         </div>
-        <img src="@/static/mp-weixin/home-page/warn.png" />
+        <img src="@/static/mp-weixin/home-page/selectright.png" />
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <div class="cycle-box">
+          <div class="cycle">
+            <img src="~@/static/mp-weixin/home-page/scycle.png" />
+            <div class="cycle-op">
+              <div class="cycle-jieneng">28</div>
+              <div class="cycle-jietext">节能率</div>
+            </div>
+          </div>
+          <div class="sort-timing">
+            <div class="timing">
+              <div class="react-text">3.8</div>
+              <div class="react-desc">实时COP</div>
+            </div>
+            <div class="timing">
+              <div class="react-text">3.8</div>
+              <div class="react-desc">实时COP</div>
+            </div>
+          </div>
+        </div>
+        <div class="other-box">
+          <div class="local-engc otherdivstyle">
+            <div class="react-text">3.8</div>
+            <div class="react-desc">实时COP</div>
+          </div>
+          <div class="local-error otherdivstyle">
+            <div class="react-text-red">3.8</div>
+            <div class="react-desc">实时COP</div>
+          </div>
+        </div>
+      </div>
+      <div class="handle-center">
+        <div class="comhandelstyle">
+          <img src="~@/static/mp-weixin/home-page/check.png" />
+          <div class="handle-center-text">
+            <div class="handle-center-hand">
+              <div>实时监测</div>
+              <div>在线</div>
+            </div>
+            <div class="handle-center-hand">
+              <div class="handle-myutext">群控关闭</div>
+              <div class="handle-myutext">今日运行时长：100h</div>
+            </div>
+          </div>
+        </div>
+        <div class="comhandelstyle">
+          <img src="~@/static/mp-weixin/home-page/datacenter.png" />
+          <div class="handle-center-text">
+            <div class="handle-center-hand">
+              <div>数据中心</div>
+              <div>正常</div>
+            </div>
+            <div class="handle-center-hand">
+              <div class="handle-myutext">今日能耗：20000kmh</div>
+              <div class="handle-myutext">设备总数：200台</div>
+            </div>
+          </div>
+        </div>
+        <div class="comhandelstyle">
+          <img src="~@/static/mp-weixin/home-page/plice-center.png" />
+          <div class="handle-center-text">
+            <div class="handle-center-hand">
+              <div>报警中心</div>
+              <div>严重：0</div>
+            </div>
+            <div class="handle-center-hand">
+              <div class="handle-myutext">紧急：0</div>
+              <div class="handle-myutext">提示：0</div>
+              <div class="handle-myutext">一般：0</div>
+            </div>
+          </div>
+        </div>
+        <div class="comhandelstyle">
+          <img src="~@/static/mp-weixin/home-page/timing.png" />
+          <div class="handle-center-text">
+            <div class="handle-center-hand">
+              <div>智能定时</div>
+              <div>启用中</div>
+            </div>
+            <div class="handle-center-hand">
+              <div></div>
+              <div class="handle-myutext">定时条数：周定时</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <van-popup
       :show="popupShow"
       position="bottom"
       custom-style="height: 40%;"
       round
-      closeable
       @close="onClosepopup">
-      内容
       <div>
         <van-picker
+          show-toolbar
+          title="请选择"
           :columns="columns"
-          @change="onChange" />
+          @cancel="handleonCancel"
+          @confirm="handleonConfirm" />
       </div>
     </van-popup>
   </div>
 </template>
 <script setup lang="ts">
 import { onLoad } from '@dcloudio/uni-app'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 import AMapWX from '@/common/amap-wx.js'
 const AampWX = ref('a57256414435de85874911887f3b47f6')
 const amapPlugin = ref()
@@ -57,6 +145,7 @@ const temperature = ref(15)
 const weather = ref('晴')
 const popupShow = ref(false)
 const columns = ref(['杭州', '宁波', '温州', '嘉兴', '湖州'])
+const chooselocal = ref('海尔空气楼c02')
 //路径更改
 onLoad(() => {
   amapPlugin.value = new AMapWX({
@@ -65,13 +154,20 @@ onLoad(() => {
   getRegeo()
   getWeather()
 })
-const onChange = (event: any) => {
-  const { picker, value, index } = event.detail
-  console.log(picker, value, index)
+const handleonCancel = (event: any) => {
+  const { value, index } = event.detail
+  console.log(value, index)
+  chooselocal.value = value
+  popupShow.value = false
+}
+const handleonConfirm = (event: any) => {
+  const { value, index } = event.detail
+  console.log(value, index)
+  chooselocal.value = value
+  popupShow.value = false
 }
 const onClosepopup = () => {
   popupShow.value = false
-  console.log(1111)
 }
 const getRegeo = () => {
   amapPlugin.value.getRegeo({
@@ -120,6 +216,11 @@ const handleChangeLocal = () => {
       height: 40rpx;
       margin-right: 12rpx;
     }
+    .local-choose {
+      width: 40rpx;
+      height: 40rpx;
+      margin-left: 10rpx;
+    }
   }
   .real-location {
     font-size: 28rpx;
@@ -146,7 +247,6 @@ const handleChangeLocal = () => {
   border-radius: 8rpx 8rpx 0rpx 0rpx;
   margin-top: 48rpx;
   padding: 0 16rpx;
-  border: 2rpx solid red;
   background-color: #fff;
   .home-weibao {
     display: flex;
@@ -165,6 +265,125 @@ const handleChangeLocal = () => {
     image {
       width: 40rpx;
       height: 40rpx;
+    }
+  }
+  .cycle-box {
+    height: 334rpx;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    .cycle {
+      width: 718rpx;
+      height: 334rpx;
+      border-radius: 8rpx 8rpx 0rpx 0rpx;
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      .cycle-jieneng {
+        font-size: 56rpx;
+        font-weight: 500;
+        color: #2681ff;
+        line-height: 80rpx;
+      }
+      .cycle-jietext {
+        font-size: 28rpx;
+        font-weight: 400;
+        color: #333333;
+        line-height: 40rpx;
+      }
+      image {
+        width: 334rpx;
+        height: 334rpx;
+        position: absolute;
+      }
+    }
+    .sort-timing {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+    .timing {
+      width: 334rpx;
+      height: 158rpx;
+      background: linear-gradient(125deg, rgba(38, 129, 255, 0.04) 0%, rgba(38, 129, 255, 0.15) 100%);
+      border-radius: 4rpx;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      .react-text {
+        font-size: 40rpx;
+        font-weight: 500;
+        color: #333333;
+        line-height: 56rpx;
+      }
+    }
+  }
+  .other-box {
+    display: flex;
+    justify-content: space-between;
+    height: 158rpx;
+    margin-top: 18rpx;
+    .otherdivstyle {
+      width: 334rpx;
+      height: 158rpx;
+      border-radius: 4rpx;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      .react-text {
+        font-size: 40rpx;
+        font-weight: 500;
+        color: #333333;
+        line-height: 56rpx;
+      }
+      .react-text-red {
+        font-size: 40rpx;
+        font-weight: 500;
+        color: #f05537;
+        line-height: 56rpx;
+      }
+    }
+    .local-engc {
+      background: linear-gradient(90deg, rgba(38, 129, 255, 0.15) 0%, rgba(38, 199, 255, 0.04) 100%);
+    }
+    .local-error {
+      background: linear-gradient(90deg, rgba(240, 85, 55, 0.15) 0%, rgba(240, 85, 55, 0.04) 100%);
+    }
+  }
+}
+.handle-center {
+  margin-top: 32rpx;
+  padding-bottom: 32rpx;
+  .comhandelstyle {
+    // width: 100%;
+    height: 168rpx;
+    box-shadow: 0rpx 0rpx 10rpx 0rpx rgba(0, 0, 0, 0.1);
+    border-radius: 4rpx;
+    margin-bottom: 16rpx;
+    padding: 0 32rpx;
+    display: flex;
+    align-items: center;
+    image {
+      width: 88rpx;
+      height: 88rpx;
+      margin-right: 40rpx;
+    }
+    .handle-center-text {
+      flex: 1;
+      .handle-myutext {
+        color: #333333;
+        font-size: 28rpx;
+      }
+    }
+    .handle-center-hand {
+      margin-bottom: 8rpx;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     }
   }
 }
